220b71494661762c2f9d2c947dfb6ec3698b34ba
Stewart Young <stewartyoung97@gmail.com>
2021-03-11 00:24:03 +0000

    add check for high card

--- 10/PokerHands.cpp
+++ 10/PokerHands.cpp
@@ -13,7 +13,7 @@ struct Player
     unordered_map<string, int> card_suits_counts;
 } Alice, Bob;
 
-void populate_hands(string input_game, Player &player1, Player &player2)
+void populate_hands(string input_game, Player &player1, Player &player2, bool verbose = false)
 {
     ifstream infile(input_game);
     vector<string> lines;
@@ -27,19 +27,21 @@ void populate_hands(string input_game, Player &player1, Player &player2)
     // populate player hands vector
     for (vector<string>::const_iterator i = lines.begin(); i != lines.end(); ++i)
     {
-        cout << *i << "\n";
+        if (verbose)
+        {
+            cout << *i << "\n";
+        }
         string line = *i;
         player1.player_hands.push_back(line.substr(0, 14));
         player2.player_hands.push_back(line.substr(15));
     }
 }
 
-void count_value(Player &player)
+void count_value(Player &player, bool verbose = false)
 {
     // for hand in total hand history
     for (vector<string>::const_iterator i = player.player_hands.begin(); i != player.player_hands.end(); ++i)
     {
-        cout << *i << "\n";
         player.card_value_counts["2"] = 0;
         player.card_value_counts["3"] = 0;
         player.card_value_counts["4"] = 0;
@@ -48,7 +50,7 @@ void count_value(Player &player)
         player.card_value_counts["7"] = 0;
         player.card_value_counts["8"] = 0;
         player.card_value_counts["9"] = 0;
-        player.card_value_counts["10"] = 0;
+        player.card_value_counts["T"] = 0;
         player.card_value_counts["J"] = 0;
         player.card_value_counts["Q"] = 0;
         player.card_value_counts["K"] = 0;
@@ -59,20 +61,23 @@ void count_value(Player &player)
         player.card_value_counts[string(1, i[0][6])]++;
         player.card_value_counts[string(1, i[0][9])]++;
         player.card_value_counts[string(1, i[0][12])]++;
-        for (auto &it : player.card_value_counts)
+        if (verbose)
         {
-            cout << it.first << ":" << it.second << " ";
+            cout << *i << "\n";
+            for (auto &it : player.card_value_counts)
+            {
+                cout << it.first << ":" << it.second << " ";
+            }
+            cout << endl;
         }
-        cout << endl;
     }
 }
 
-void count_suits(Player &player)
+void count_suits(Player &player, bool verbose = false)
 {
     // for hand in total hand history
     for (vector<string>::const_iterator i = player.player_hands.begin(); i != player.player_hands.end(); ++i)
     {
-        cout << *i << "\n";
         player.card_suits_counts["C"] = 0;
         player.card_suits_counts["D"] = 0;
         player.card_suits_counts["H"] = 0;
@@ -83,11 +88,15 @@ void count_suits(Player &player)
         player.card_suits_counts[string(1, i[0][7])]++;
         player.card_suits_counts[string(1, i[0][10])]++;
         player.card_suits_counts[string(1, i[0][13])]++;
-        for (auto &it : player.card_suits_counts)
+        if (verbose)
         {
-            cout << it.first << ":" << it.second;
+            cout << *i << "\n";
+            for (auto &it : player.card_suits_counts)
+            {
+                cout << it.first << ":" << it.second;
+            }
+            cout << endl;
         }
-        cout << endl;
     }
 }
 
@@ -97,14 +106,42 @@ int main()
     populate_hands(string("poker.txt"), Alice, Bob);
 
     // "parse" the hand
-    // std::map of cards to worth e.g. {"2":2, ..., "A": 14}
-    // std::map of card values to count in players hand
+    // std::unordered_map of card values to count in players hand
     count_value(Alice);
     count_value(Bob);
-    // std::map of suits to their counts
+    // std::unordered_map of suits to their counts
     count_suits(Alice);
     count_suits(Bob);
+
     // calculate "value" of the hand
+
+    // high card: all value_counts < 2
+    bool high_card = true;
+    for (auto &it : Alice.card_value_counts)
+    {
+        if (it.second >= 2)
+        {
+            high_card = false;
+            cout << "not a high card\n";
+            break;
+        }
+    }
+
+    // one pair: only one value_counts == 2, all others <= 1
+
+    // two pair: two value_counts == 2
+
+    // three of a kind: only one value_counts == 3, all others <=1
+
+    // flush: a suit_counts == 5
+
+    // full house: one value_counts == 3, one value_counts == 2
+
+    // four of a kind: one value_counts == 4
+
+    // straight flush: all cards are consecutive values and suit_count == 5
+
+    // royal flush: T, J, Q, K, A and suit_count == 5
 
     // declare winner
 
